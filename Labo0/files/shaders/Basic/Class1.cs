672cd0c5e5f426b2a89a4de4d0e567a31be5fd75
Alejandro Perez Rago <ale_pr87@hotmail.com>
2017-06-15 18:28:36 -0300

    ProyBase

--- Labo0/files/shaders/Basic/Class1.cs
+++ Labo0/files/shaders/Basic/Class1.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Labo0.files.TextShaders.Basic
+namespace Labo0.files.shaders.Basic
 {
     class Class1
     {
-        int hola;
     }
 }
